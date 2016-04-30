87bd37b562c7dabfae2a5f4faabfeb0d3af3a911
Brigham Keys, Esq <bkeys@gnu.org>
2016-04-30 16:30:25 -0500

    We can never do shit the easy way

--- VehicleDemo/src/main.cpp
+++ VehicleDemo/src/main.cpp
@@ -1,14 +1,57 @@
 #include "VehicleDemo.h"
+#include "Globals.hpp"
+#include "Func.hpp"
 #include <btBulletDynamicsCommon.h>
+#include <irrlicht.h>
 
-int main(int argc,char** argv) {
+int main(int argc, char *argv[]) {
+
+    // Irrlicht grabs the cursor so it is not in the way of our visualisation
+    // Window caption gets set.
+    device->getCursorControl()->setVisible(0);
+    device->setWindowCaption(L"Lesson 01 - Hello Bullet");
+
+    // Set the camera backwards and viewing the origin; setting up the visualisation
+    cam->setPosition(irr::core::vector3df(0, 2, -5));
+    cam->setTarget(irr::core::vector3df(0, 2, 0));
+    smgr->addLightSceneNode(0, irr::core::vector3df(2, 10, -2), irr::video::SColorf(4, 4, 4, 1));
+
+    // Creating our scene
+    createScene();
+
+    // Use a timestamp to regulate the frames, and stepping the simulation
+    irr::u32 timeStamp = timer->getTime(), deltaTime = 0;
 
     VehicleDemo* vehicleDemo = new VehicleDemo;
 
     vehicleDemo->initPhysics(); 
 
-    while(true) {
-	vehicleDemo->clientMoveAndDisplay();
-    }
-}
+    // Game loop
+    while(!done) {
 
+	// Code to record when the frame began
+	deltaTime = timer->getTime() - timeStamp;
+	timeStamp = timer->getTime();
+
+	// Step the simulation
+	updatePhysics(deltaTime);
+
+	vehicleDemo->clientMoveAndDisplay();
+	// Irrlicht draws the scene
+	driver->beginScene(true, true, irr::video::SColor(0, 15, 192, 252));
+	smgr->drawAll();
+	driver->endScene();
+	device->run();
+    }
+
+    // clean up
+    clearObjects();
+    delete world;
+    delete solver;
+    delete dispatcher;
+    delete broadphase;
+    delete collisionConfiguration;
+    device->drop();
+
+   return 0;
+}
